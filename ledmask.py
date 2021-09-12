b5bbfbfbce81f77f9e784ca3125b5a8f171e4d42
kasamikona <KasaneKona@gmail.com>
2021-09-12 01:16:39 +0100

    Handle errors and scan differently

--- ledmask.py
+++ ledmask.py
@@ -2,6 +2,8 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 from Crypto.Cipher import AES
 from displays import DSD
+from bleak.exc import BleakError
+import System
 
 cipher = None
 display = None
@@ -18,14 +20,15 @@ def decrypt(packet):
 
 async def get_device():
     global display, cipher
-    devices = await BleakScanner.discover()
     dev = None
     display = None
     cipher = None
-    for d in devices:
-        if check_setup_device(d):
-            dev = d
-            break
+    async with BleakScanner() as scanner:
+        await asyncio.sleep(2) # 2 seconds should be long enough
+        for d in scanner.discovered_devices:
+            if check_setup_device(d):
+                dev = d
+                break
     return dev
 
 def check_setup_device(device):
@@ -39,26 +42,32 @@ def check_setup_device(device):
     return True
 
 async def run():
-    device = await get_device()
-    if not device:
-        print("No device found")
-        return
-    
-    print("Found %s (%s), connecting" % (device.name, device.address))
-    async with BleakClient(device.address) as client:
-        print("Connected, begin main program")
-        await sendtest(client)
-        print("Done")
-    print("Disconnected")
+    try:
+        print("Looking for compatible device")
+        device = await get_device()
+        if not device:
+            print("No device found")
+            return
+        
+        print("Found %s (%s), connecting" % (device.name, device.address))
+        async with BleakClient(device.address) as client:
+            print("Connected, begin main program")
+            await sendtest(client)
+            print("Done")
+        print("Disconnected")
+    except (BleakError, System.Exception) as err:
+        print(err)
+        print("\n=========================================================================\n")
+        print("General Bluetooth error, is your adapter connected and enabled?")
 
 async def sendtest(client):
     await client.write_gatt_char(display.char_cmd, bytearray.fromhex(encrypt("0844 4154 5300 4800 0000 0000 0000 0000"))) # DATS
     await asyncio.sleep(0.5) # Hack because I cba to wait for DATSOK
-    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ff00 0000"))) # data
-    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ff00 0000"))) # data
-    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ff00 0000"))) # data
-    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ff00 0000"))) # data
-    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0cff ffff ffff ffff ffff ffff ff00 0000"))) # data
+    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ffff ffff"))) # data
+    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ffff ffff"))) # data
+    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ffff ffff"))) # data
+    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0fff ffff ffff ffff ffff ffff ffff ffff"))) # data
+    await client.write_gatt_char(display.char_dat, bytearray.fromhex(encrypt("0cff ffff ffff ffff ffff ffff ffff ffff"))) # data
     await client.write_gatt_char(display.char_cmd, bytearray.fromhex(encrypt("0544 4154 4350 0000 0000 0000 0000 0000"))) # DATCP
     await asyncio.sleep(0.5) # Hack because I cba to wait for DATCPOK
     await client.write_gatt_char(display.char_cmd, bytearray.fromhex(encrypt("054d 4f44 4501 0000 0000 0000 0000 0000"))) # MODE
