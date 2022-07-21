73476368f22f21c24c3ffc96239615a915d78fad
kasamikona <kasamikona@gmail.com>
2022-07-21 21:11:29 +0100

    Fix working path intro.sh

--- tools/make_intro.sh
+++ tools/make_intro.sh
@@ -3,7 +3,7 @@ tools=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd tools/..
 rm -rf displays/__pycache__
 rm -rf effects/__pycache__
-echo "a=/tmp/genshiken;b=\$a/intro.tar.gz;echo Unpacking to \$a;mkdir \$a;tail -n+2 \$0>\$b;tar xzvf \$b -C \$a 2>/dev/null;echo;${PYTHON:=\"python\"} \$a/bitmask.py \$1;echo Cleaning up \$a;rm -rf \$a;exit" > $dest/intro.sh
+echo "a=/tmp/genshiken;b=\$a/intro.tar.gz;echo Unpacking to \$a;mkdir \$a;tail -n+2 \$0>\$b;tar xzvf \$b -C \$a 2>/dev/null;echo;cd \$a;${PYTHON:=\"python\"} bitmask.py \$1;echo Cleaning up \$a;rm -rf \$a;exit" > $dest/intro.sh
 tar cvf - displays effects ba.bin bitmask.nsf bitmask.py demo.story kgfx.py ledmask.py | gzip -9 - >> $dest/intro.sh
 offset=$( stat --printf="%s" $dest/intro.sh )
 echo "aaaa" >> $dest/intro.sh
